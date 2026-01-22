9205abb197ce4ad279f35767167288993ea35c8b
Davor Starček <davor.starcek@mstart.eu>
2026-01-22 17:44:07 +0100

    ds

--- obj/Debug/net8.0/KarlixID.Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/KarlixID.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarlixID.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33af547c888fe72e83f20088aa90f58bcc6dbe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a483877339b61818fe60afc80502501599cd5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarlixID.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarlixID.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
